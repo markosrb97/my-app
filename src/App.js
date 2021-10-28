74f76a845cce76dfb9443d6ae619d5e1738bf19b
markosrb97 <marko.radosavljevic97@gmail.com>
2021-10-28 18:09:31 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="App">
+      <div className="container">
+        <section className="image-half">
+          <div className="woman-svg"></div>
+          <div className="shadow-svg"></div>
+        </section>
+        <section className="text-container">
+          <h1>FAQ</h1>
+          <div className="questioin-block">
+            <div className="question-and-answer">
+              <p className="question">How many team members can I invite?</p>
+              <p className="answer">
+                You can invite up to 2 additional users on the Free plan. There
+                is no limit on team members for the Premium plan.
+              </p>
+            </div>
+            <div className="question-and-answer">
+              <p className="question">How many team members can I invite?</p>
+              <p className="answer">
+                You can invite up to 2 additional users on the Free plan. There
+                is no limit on team members for the Premium plan.
+              </p>
+            </div>
+            <div className="question-and-answer">
+              <p className="question">How many team members can I invite?</p>
+              <p className="answer">
+                You can invite up to 2 additional users on the Free plan. There
+                is no limit on team members for the Premium plan.
+              </p>
+            </div>
+            <div className="question-and-answer">
+              <p className="question">How many team members can I invite?</p>
+              <p className="answer">
+                You can invite up to 2 additional users on the Free plan. There
+                is no limit on team members for the Premium plan.
+              </p>
+            </div>
+            <div className="question-and-answer">
+              <p className="question">How many team members can I invite?</p>
+              <p className="answer">
+                You can invite up to 2 additional users on the Free plan. There
+                is no limit on team members for the Premium plan.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </body>
   );
 }
 
